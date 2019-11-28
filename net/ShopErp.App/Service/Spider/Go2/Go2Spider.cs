47c1b442d9d8c42765acd00fe6e6a4f45ff5cf40
largesky mengsk <largesky@hotmail.com>
2019-11-28 14:55:17 +0800

    GO2获取商品时不读取类型，因为读取时经常出错

--- net/ShopErp.App/Service/Spider/Go2/Go2Spider.cs
+++ net/ShopErp.App/Service/Spider/Go2/Go2Spider.cs
@@ -86,7 +86,7 @@ namespace ShopErp.App.Service.Spider.Go2
 
         public override Goods GetGoodsInfoByUrl(string url, ref string vendorHomePage, ref string videoUrl, bool raiseExceptionOnGoodsNotSale, bool getGoodsType)
         {
-            Goods g = new Goods { Comment = "", CreateTime = DateTime.Now, Image = "", LastSellTime = DateTime.Now, Number = "", Price = 0, Type = 0, UpdateEnabled = true, UpdateTime = DateTime.Now, Url = url, VendorId = 0, Weight = 0, Id = 0, Colors = "", CreateOperator = "", Flag = ColorFlag.UN_LABEL, IgnoreEdtion = false, ImageDir = "", Material = "", Shops = new List<GoodsShop>(), Star = 0, VideoType = GoodsVideoType.NONE };
+            Goods g = new Goods { Comment = "", CreateTime = DateTime.Now, Image = "", LastSellTime = DateTime.Now, Number = "", Price = 0, Type = 0, UpdateEnabled = true, UpdateTime = DateTime.Now, Url = url, VendorId = 0, Weight = 0, Id = 0, Colors = "", CreateOperator = "", Flag = ColorFlag.UN_LABEL, IgnoreEdtion = false, ImageDir = "", Material = "", Shops = new List<GoodsShop>(), Star = 0, VideoType = GoodsVideoType.NONE};
             var htmlDoc = this.GetHtmlDocWithRetry(url);
 
             //商品已删除 
@@ -186,11 +186,6 @@ namespace ShopErp.App.Service.Spider.Go2
                     g.VideoType = GoodsVideoType.NOT;
                     videoUrl = "";
                 }
-            }
-
-            if (getGoodsType)
-            {
-                g.Type = GetGoodsType(g.Url, vendorHomePage);
             }
             var colorNode = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='details-item']/div/ul/li[@class='details-attribute-item props-color']");
             if (colorNode != null)
@@ -347,60 +342,6 @@ namespace ShopErp.App.Service.Spider.Go2
                 if (this.IsStop == false)
                     this.GetVendors(c, vendors);
             }
-        }
-
-        private GoodsType GetGoodsType(string goodsUrl, string vendorHomePage)
-        {
-            string supplrUrl = vendorHomePage + "/supply.html";
-            Uri uri = new Uri(goodsUrl);
-            string html = MsHttpRestful.GetUrlEncodeBodyReturnString(supplrUrl, null);
-            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-            doc.LoadHtml(html);
-
-            var nodes = doc.DocumentNode.SelectNodes("//li[@class='second-nav-item']/a");
-            if (nodes.Count < 1)
-            {
-                throw new Exception("无法自动分析商品类型，原因：网页中没有分类连接");
-            }
-
-            foreach (var v in nodes)
-            {
-                string onlick = v.GetAttributeValue("onclick", "");
-                string value = v.InnerText;
-
-                string cat = onlick.Replace("changeCat('", "").Replace("')", "").Replace(";", "");
-                string type = value.Substring(0, value.IndexOf("(")).Trim();
-
-                if (type.Contains("所有"))
-                {
-                    continue;
-                }
-
-                string lasthtml = "";
-                int i = 1;
-                while (true)
-                {
-                    string url = string.Format("{0}/products/supply-{1}-{2}-6", vendorHomePage, cat, i++);
-                    string goodsHtml = MsHttpRestful.GetUrlEncodeBodyReturnString(url, null);
-
-                    if (string.IsNullOrWhiteSpace(goodsHtml))
-                    {
-                        break;
-                    }
-
-                    if (goodsHtml.Contains(uri.AbsolutePath))
-                    {
-                        return FormatType(type);
-                    }
-
-                    if (lasthtml != "" && lasthtml == goodsHtml)
-                    {
-                        break;
-                    }
-                    lasthtml = goodsHtml;
-                }
-            }
-            return GoodsType.GOODS_SHOES_NONE;
         }
     }
 }
