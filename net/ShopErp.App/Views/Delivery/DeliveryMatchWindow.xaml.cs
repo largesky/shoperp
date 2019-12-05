b62ce7470ad5f40faaa7653903cdc94f54edf55d
largesky mengsk <largesky@hotmail.com>
2019-12-05 23:55:18 +0800

    更新发货记录比较

--- net/ShopErp.App/Views/Delivery/DeliveryMatchWindow.xaml.cs
+++ net/ShopErp.App/Views/Delivery/DeliveryMatchWindow.xaml.cs
@@ -31,47 +31,11 @@ namespace ShopErp.App.Views.Delivery
 
         private string AnDetail(string[] deliveryNumbers)
         {
-            var allNumbers = deliveryNumbers.Where(obj => string.IsNullOrWhiteSpace(obj) == false);
-            List<string> eNumbers = new List<string>(); //电子面单没有段号，
-            List<long> dNumbers = new List<long>(); //货到付款订单
-            foreach (var s in allNumbers)
-            {
-                if (s.StartsWith("D", StringComparison.OrdinalIgnoreCase))
-                {
-                    dNumbers.Add(long.Parse(s.TrimStart('D', 'd')));
-                }
-                else
-                {
-                    eNumbers.Add(s);
-                }
-            }
-            //计算普通起始单号，个数
-            eNumbers.Sort();
-            dNumbers.Sort();
-            var start = dNumbers.Select(obj => obj / 1000);
-            Dictionary<long, int> ccD = new Dictionary<long, int>();
-            foreach (var l in dNumbers.Distinct())
-            {
-                var lt = l / 1000;
-                if (ccD.ContainsKey(lt))
-                {
-                    ccD[lt]++;
-                }
-                else
-                {
-                    ccD[lt] = 1;
-                }
-            }
+            var allNumbers = deliveryNumbers.Where(obj => string.IsNullOrWhiteSpace(obj) == false).ToList();
+            allNumbers.Sort();
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("文件内总数：" + allNumbers.Count());
             sb.AppendLine("有效总数：" + allNumbers.Distinct().Count());
-            sb.AppendLine(string.Format("普通面单代收:总数 {0},有效总数 {1}", dNumbers.Count, dNumbers.Distinct().Count()));
-            sb.AppendLine(string.Format("电子面单:总数 {0},有效总数 {1}", eNumbers.Count, eNumbers.Distinct().Count()));
-            sb.AppendLine("普通面单代收明细:");
-            foreach (var c in ccD)
-            {
-                sb.AppendLine(c.Key + "001  个数:" + c.Value);
-            }
             return sb.ToString();
         }
 
@@ -87,7 +51,7 @@ namespace ShopErp.App.Views.Delivery
             return -1;
         }
 
-        private FileDeliveryInfo[] ReadFile(string file)
+        private string[] ReadFile(string file)
         {
             ExcelFile fr = ExcelFile.Open(file);
             string[][] contents = fr.ReadFirstSheet();
@@ -108,7 +72,7 @@ namespace ShopErp.App.Views.Delivery
             {
                 throw new Exception("文件中没有找到 重量 列:" + file);
             }
-            List<FileDeliveryInfo> fdis = new List<FileDeliveryInfo>();
+            List<string> fdis = new List<string>();
             for (int i = 1; i < contents.Length; i++)
             {
                 string[] content = contents[i];
@@ -120,28 +84,14 @@ namespace ShopErp.App.Views.Delivery
                 {
                     continue;
                 }
-
-                try
-                {
-                    var fdi = new FileDeliveryInfo
-                    {
-                        DeliveryNumber = content[dnIndex],
-                        Weight = float.Parse(content[wIndex]),
-                        Money = 0,
-                    };
-                    fdis.Add(fdi);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
+                fdis.Add(content[dnIndex]);
             }
             return fdis.ToArray();
         }
 
-        private FileDeliveryInfo[] ReadFiles(string[] files)
+        private string[] ReadFiles(string[] files)
         {
-            var fdis = new List<FileDeliveryInfo>();
+            var fdis = new List<string>();
             foreach (var file in files)
             {
                 try
@@ -162,38 +112,24 @@ namespace ShopErp.App.Views.Delivery
             try
             {
                 //分析统计
-                var fdis = this.ReadFiles(this.Files);
-                var dnSystem = this.DeliveryOuts.Select(obj => obj.DeliveryNumber).ToArray();
-                var dnFile = fdis.Select(obj => obj.DeliveryNumber).ToArray();
-
-                string str = AnDetail(dnSystem);
-                this.tbSystemDetail.Text = str;
-
-                str = AnDetail(dnFile);
-                this.tbFileDetail.Text = str;
-
+                var dnSystem = this.DeliveryOuts.Select(obj => obj.DeliveryNumber).Distinct().ToList();
+                var dnFile = this.ReadFiles(this.Files).Distinct().ToList();
                 List<DeliveryMatchDetail> matchDds = new List<DeliveryMatchDetail>();
                 List<DeliveryMatchDetail> unmatchDds = new List<DeliveryMatchDetail>();
-                var dnnSystem = this.DeliveryOuts.Distinct(new DeliveryOutComparer())
-                    .OrderBy(obj => long.Parse(obj.DeliveryNumber.TrimStart('D', 'd'))).ToArray();
-                var dnnFile = fdis.Distinct(new FileDeliveryInfoComparer())
-                    .OrderBy(obj => long.Parse(obj.DeliveryNumber.TrimStart('D', 'd'))).ToArray();
-
                 int i = 0, j = 0;
-                for (; i < dnnSystem.Length && j < dnnFile.Length;)
+
+                dnSystem.Sort();
+                dnFile.Sort();
+
+                for (; i < dnSystem.Count && j < dnFile.Count;)
                 {
-                    long diff = long.Parse(dnnSystem[i].DeliveryNumber.TrimStart('D', 'd')) -
-                                long.Parse(dnnFile[j].DeliveryNumber.TrimStart('D', 'd'));
+                    long diff = dnSystem[i].CompareTo(dnFile[j]);
                     if (diff == 0)
                     {
                         var ddd = new DeliveryMatchDetail
                         {
-                            OrderId = dnnSystem[i].OrderId,
-                            DeliveryNumberSystem = dnnSystem[i].DeliveryNumber,
-                            WeightSystem = dnnSystem[i].Weight,
-                            DeliveryNumberFile = dnnFile[j].DeliveryNumber,
-                            WeightFile = dnnFile[j].Weight,
-                            MoneyFile = dnnFile[j].Money,
+                            DeliveryNumberSystem = dnSystem[i],
+                            DeliveryNumberFile = dnFile[j],
                         };
                         matchDds.Add(ddd);
                         i++;
@@ -203,12 +139,8 @@ namespace ShopErp.App.Views.Delivery
                     {
                         var ddd = new DeliveryMatchDetail
                         {
-                            OrderId = "",
                             DeliveryNumberSystem = "",
-                            WeightSystem = 0,
-                            DeliveryNumberFile = dnnFile[j].DeliveryNumber,
-                            WeightFile = dnnFile[j].Weight,
-                            MoneyFile = dnnFile[j].Money,
+                            DeliveryNumberFile = dnFile[j],
                         };
                         j++;
                         unmatchDds.Add(ddd);
@@ -217,12 +149,8 @@ namespace ShopErp.App.Views.Delivery
                     {
                         var ddd = new DeliveryMatchDetail
                         {
-                            OrderId = dnnSystem[i].OrderId,
-                            DeliveryNumberSystem = dnnSystem[i].DeliveryNumber,
-                            WeightSystem = dnnSystem[i].Weight,
+                            DeliveryNumberSystem = dnSystem[i],
                             DeliveryNumberFile = "",
-                            WeightFile = 0,
-                            MoneyFile = 0,
                         };
                         i++;
                         unmatchDds.Add(ddd);
@@ -243,6 +171,7 @@ namespace ShopErp.App.Views.Delivery
                 {
                     MessageBox.Show("数据成功完全匹配", "恭喜", MessageBoxButton.OK, MessageBoxImage.None);
                 }
+                this.tbSuminfo.Text = string.Format("系统发货记录共：{0}条，文件发货记录共：{1}条", dnSystem.Count, dnFile.Count);
             }
             catch (Exception ex)
             {
@@ -252,62 +181,11 @@ namespace ShopErp.App.Views.Delivery
         }
     }
 
-
-    public class FileDeliveryInfo
-    {
-        public string DeliveryNumber { get; set; }
-
-        public float Weight { get; set; }
-
-        public float Money { get; set; }
-    }
-
     public class DeliveryMatchDetail
     {
         public int RowId { get; set; }
-
-        public string OrderId { get; set; }
-
-        public float WeightSystem { get; set; }
-
         public string DeliveryNumberSystem { get; set; }
-
-        public float WeightFile { get; set; }
-
         public string DeliveryNumberFile { get; set; }
-
-        public float MoneyFile { get; set; }
-
-        public float WeightDiff
-        {
-            get { return WeightSystem - WeightFile; }
-        }
     }
 
-
-    public class DeliveryOutComparer : IEqualityComparer<DeliveryOut>
-    {
-        public bool Equals(DeliveryOut x, DeliveryOut y)
-        {
-            return x.DeliveryNumber == y.DeliveryNumber;
-        }
-
-        public int GetHashCode(DeliveryOut obj)
-        {
-            return obj.DeliveryNumber.GetHashCode();
-        }
-    }
-
-    public class FileDeliveryInfoComparer : IEqualityComparer<FileDeliveryInfo>
-    {
-        public bool Equals(FileDeliveryInfo x, FileDeliveryInfo y)
-        {
-            return x.DeliveryNumber == y.DeliveryNumber;
-        }
-
-        public int GetHashCode(FileDeliveryInfo obj)
-        {
-            return obj.DeliveryNumber.GetHashCode();
-        }
-    }
 }
