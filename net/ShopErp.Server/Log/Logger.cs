a375b18cd82688c792f1f4b149d0610427a18e0e
largesky mengsk <largesky@hotmail.com>
2020-04-09 20:23:29 +0800

    日志写的时候加锁，不然多线程会出错

--- net/ShopErp.Server/Log/Logger.cs
+++ net/ShopErp.Server/Log/Logger.cs
@@ -12,18 +12,24 @@ namespace ShopErp.Server.Log
 
         public static void Log(string title, Exception ex)
         {
-            string fileName = System.IO.Path.Combine(Utils.EnvironmentDirHelper.DIR_LOG, FILE_PREFIX + DateTime.Now.ToString("_yyyy_MM_dd") + ".txt");
-            System.IO.File.AppendAllText(fileName, title, Encoding.Default);
-            System.IO.File.AppendAllText(fileName, ex.Message, Encoding.Default);
-            System.IO.File.AppendAllText(fileName, ex.StackTrace, Encoding.Default);
+            lock (FILE_PREFIX)
+            {
+                string fileName = System.IO.Path.Combine(Utils.EnvironmentDirHelper.DIR_LOG, FILE_PREFIX + DateTime.Now.ToString("_yyyy_MM_dd") + ".txt");
+                System.IO.File.AppendAllText(fileName, title, Encoding.Default);
+                System.IO.File.AppendAllText(fileName, ex.Message, Encoding.Default);
+                System.IO.File.AppendAllText(fileName, ex.StackTrace, Encoding.Default);
+            }
         }
 
         public static void Log(params string[] msgs)
         {
-            string fileName = System.IO.Path.Combine(Utils.EnvironmentDirHelper.DIR_LOG, FILE_PREFIX + DateTime.Now.ToString("_yyyy_MM_dd") + ".txt");
-            foreach (var s in msgs)
+            lock (FILE_PREFIX)
             {
-                System.IO.File.AppendAllText(fileName, s, Encoding.Default);
+                string fileName = System.IO.Path.Combine(Utils.EnvironmentDirHelper.DIR_LOG, FILE_PREFIX + DateTime.Now.ToString("_yyyy_MM_dd") + ".txt");
+                foreach (var s in msgs)
+                {
+                    System.IO.File.AppendAllText(fileName, s, Encoding.Default);
+                }
             }
         }
     }
